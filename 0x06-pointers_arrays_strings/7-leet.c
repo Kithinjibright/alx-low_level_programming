27aa7352efd719db8c83e52a5469eaf23389334f
Kithinjibright <kithinjibrighton31@gmail.com>
2022-03-24 11:15:43 -0700

    Function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,7 +11,6 @@ char *leet(char *s)
 	int i, x;
 	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int replacer[] = {'4', '3', '0', '7', '1'};
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (x = 0; x <= 9; x++)
