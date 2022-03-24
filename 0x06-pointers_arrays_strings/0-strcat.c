575e7f4f52ed203b81a484ad31682071fd7a4f03
Kithinjibright <kithinjibrighton31@gmail.com>
2022-03-24 06:04:21 -0700

    Function that concatenates two strings, then adds a terminating null byte

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  * Return: the concatenated string
  */
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int j = 0;
@@ -21,7 +21,7 @@ char *_strcat(char *dest, char *src);
 		i++;
 		j++;
 	}
-	dest[i] = '\0;
+	dest[i] = '\0';
 
 	return (dest);
 }
