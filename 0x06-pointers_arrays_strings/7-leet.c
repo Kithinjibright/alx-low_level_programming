f947ac952a6d3ae3a3b057b2b20151b508177ff0
Kithinjibright <kithinjibrighton31@gmail.com>
2022-03-24 11:39:47 -0700

    Function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,28 @@
 #include "main.h"
-
 /**
- * leet - encode string into 1337 leet
- * @s: string to manipulate
- * Return: string
+ * leet - encodes a string into 1337
+ * @s: input string
+ * Return: to s
  */
 
 char *leet(char *s)
 {
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
-	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-
-	int i, j;
-
-	for (j = 0; s[j] != '\0'; j++)
+	while (*(s + count) != '\0')
 	{
-		for (i = 0; a[i] != '\0'; i++)
+		for (i = 0; i < 5; i++)
 		{
-			if (s[j] == a[i])
-				s[j] = b[i];
+			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
 		}
+		count++;
 	}
 
 	return (s);
