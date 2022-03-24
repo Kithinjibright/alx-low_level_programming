3b17ca7b225f515c45618f83b27271c35db5e4cf
Kithinjibright <kithinjibrighton31@gmail.com>
2022-03-24 06:24:18 -0700

    Function that concatenates two strings, does not need to be null-terminated if it contains n or more bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n bytes to destination string
+ * @dest: string being appended to
+ * @src: append this string
+ * Return: the concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0') /*i index of null terminator*/
+		i++;
+	while (src[j] != src[n]) /*appends replacing null terminator*/
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
