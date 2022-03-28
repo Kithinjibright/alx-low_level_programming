3e01566b13e6036b351ef6c9a34f6dba33775006
Kithinjibright <kithinjibrighton31@gmail.com>
2022-03-28 08:41:20 -0700

    Function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+C programming
+
+More pointers, arrays and strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: fill memory to this buffer
+ * @b: source to fill
+ * @n: n bytes to fill with
+ * Return: filled memory
+ */
+
+char *_memset(char *s, char b, unsigned int n);
