28f0984098895b6f17b16097905ea872afb1d810
Kithinjibright <kithinjibrighton31@gmail.com>
2022-03-30 13:27:54 -0700

    Function that prints a string, followed by a new line

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+C programming
+
+Recursion is a method where the solution to a problem depends on solutions to smaller instances of the same problem.
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s);
+
+#endif
