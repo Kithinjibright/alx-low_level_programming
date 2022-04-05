0fb94cf522328d6620102e31ff25a9e1120a4165
Kithinjibright <kithinjibrighton31@gmail.com>
2022-04-05 14:13:14 -0700

    Function that concatenates two strings

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -24,6 +24,14 @@ char *create_array(unsigned int size, char c);
  */
 
 char *_strdup(char *str);
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string one to concatenate
+ * @s2: string two to concatenate
+ * Return: string one and two concatenated
+ */
+
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
