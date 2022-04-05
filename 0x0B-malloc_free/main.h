3f0bcc4113808708f3547d44e32e87a2dead8fce
Kithinjibright <kithinjibrighton31@gmail.com>
2022-04-05 13:58:06 -0700

    Function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -16,6 +16,13 @@ int _putchar(char c);
  */
 
 char *create_array(unsigned int size, char c);
+
+/**
+ * _strdup - duplicates a string
+ * @str: duplicate this string
+ * Return: duplicated string
+ */
+
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
