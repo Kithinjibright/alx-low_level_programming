184d8886c0ac219217e2a02b73e90b4fe79a461a
Kithinjibright <kithinjibrighton31@gmail.com>
2022-04-07 13:43:06 -0700

    Function that concatenates two strings

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -16,4 +16,14 @@ int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
 
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: n bytes to concat
+ * Return: length of string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 #endif
