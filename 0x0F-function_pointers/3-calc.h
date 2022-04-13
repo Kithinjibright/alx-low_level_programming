c6d454c9bfa16cbe7e8a9fa85551ec67c919d64b
Kithinjibright <kithinjibrighton31@gmail.com>
2022-04-13 13:54:24 -0700

    File containing all the function prototypes and data structures used by the program

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,9 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _3_CALC_H
+#define _3_CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct op - Struct op
  *
@@ -12,10 +16,13 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+/* operations */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
 int (*get_op_func(char *s))(int, int);
+
 #endif
